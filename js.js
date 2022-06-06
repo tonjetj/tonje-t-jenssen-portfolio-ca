100d1d6914b7b198d478e9dd9969cd4ba1f7a2bd
Tonje <91598255+tonjetj@users.noreply.github.com>
2022-06-06 19:50:17 +0200

    g

---
+++ js.js
@@ -0,0 +1,17 @@
+const nav = document.querySelector("nav");
+
+function handleScroll() {
+  console.log(window.scrollY);
+
+  const scrolledY = window.scrollY;
+
+  if (scrolledY > 20) {
+    nav.classList.add("scrolled");
+  } else {
+    nav.classList.remove("scrolled");
+  }
+}
+
+window.onscroll = handleScroll;
+
+window.addEventListener("scroll", handleScroll);
