5c99c73d3698b2e0389d0f4929f2b583accb0dfd
Tonje <91598255+tonjetj@users.noreply.github.com>
2022-06-10 19:47:19 +0200

    f

--- js.js
+++ js.js
@@ -30,7 +30,7 @@ window.onscroll = function () {
 };
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
     myButton.style.display = "block";
   } else {
     myButton.style.display = "none";
