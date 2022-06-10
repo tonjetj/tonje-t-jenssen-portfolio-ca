63bc6fbb78a3aa7ad1a73c433fea6265054f4c2c
Tonje <91598255+tonjetj@users.noreply.github.com>
2022-06-10 19:45:33 +0200

    meep

--- js.js
+++ js.js
@@ -18,3 +18,21 @@ function handleScroll() {
 window.onscroll = handleScroll;
 
 window.addEventListener("scroll", handleScroll);
+
+const myButton = document.querySelector("#myBtn");
+myButton.onclick = function () {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+};
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    myButton.style.display = "block";
+  } else {
+    myButton.style.display = "none";
+  }
+}
