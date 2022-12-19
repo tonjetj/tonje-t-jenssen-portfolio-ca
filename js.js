42dba3c4ad6a3aefa4ac73784ccbb0ac844a577d
tonjetj <tontotjen@gmail.com>
2022-12-19 09:01:37 +0100

    ok

--- js.js
+++ js.js
@@ -36,3 +36,52 @@ function scrollFunction() {
     myButton.style.display = "none";
   }
 }
+
+////
+
+function handleScroll() {
+  console.log(window.scrollY);
+
+  const scrolledY = window.scrollY;
+  const about = document.querySelector(".indicator.one");
+  const work = document.querySelector(".indicator.two");
+  const contact = document.querySelector(".indicator.three");
+
+  if (scrolledY > 475) {
+    about.classList.add("scrolled");
+  } else {
+    about.classList.remove("scrolled");
+  }
+
+  if (scrolledY > 1100) {
+    work.classList.add("scrolled");
+  } else {
+    work.classList.remove("scrolled");
+  }
+
+  if (scrolledY > 4195) {
+    contact.classList.add("scrolled");
+  } else {
+    contact.classList.remove("scrolled");
+  }
+}
+
+window.onscroll = handleScroll;
+
+window.addEventListener("scroll", handleScroll);
+
+////////// Bar progress
+
+window.onscroll = function () {
+  myFunction();
+};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.height = scrolled + "%";
+  document.getElementById("myBarTwo").style.height = scrolled + "%";
+}
